cdb2206b69fc71877aaaa90f6c5a9e738036e9b4
Wesley <wesley.leoncio.izi@gmail.com>
2021-07-26 16:11:59 -0300

    Atualização na api category!

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,12 +1,31 @@
-module.exports = app =>{
-    app.route('/users')
-        .post(app.api.user.save)
-        .get(app.api.user.get);
+module.exports = app => {
+   // USERS 
+   app.route('/users')
+      .post(app.api.user.save)
+      .get(app.api.user.get);
+
+   app.route('/users/:id')
+      .put(app.api.user.save);
+
+   app.route('/users/:id')
+      .get(app.api.user.getById);
+   ///////////////////////////////////////
+
+   // Categorys
+   app.route('/categories')
+      .get(app.api.category.get)
+      .post(app.api.category.save);
 
 
-     app.route('/users/:id') 
-        .put(app.api.user.save);  
+   // Cuidado com ordem! Tem que vir antes de /categories/:id
+   app.route('/categories/tree')
+      .get(app.api.category.getTree);
 
-     app.route('/users/:id') 
-        .get(app.api.user.getById);  
+   app.route('/categories/:id')
+      .get(app.api.category.getById)
+      .put(app.api.category.save)
+      .delete(app.api.category.remove);
+
+   ///////////////////////////////////////
+
 }
