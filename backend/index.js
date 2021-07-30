c539da29fbbdac979df5dc0e3a34dd60da1f457e
Wesley <wesley.leoncio.izi@gmail.com>
2021-07-30 16:59:23 -0300

    Atualização na api de auth e implementação do jwt

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@ const db = require('./config/db');
 app.db = db;
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validation.js')
     .then('/api')
     .then('/config/routes.js')
     .into(app);
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Backend executando...');
 })
