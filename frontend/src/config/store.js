30ddf2efcda64cf6816ed02f85393a1460cea3d8
Wesley <wesley.leoncio.izi@gmail.com>
2021-08-19 20:44:25 -0300

    Atualização!

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -5,7 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:{
-        isMenuVisible: true
+        isMenuVisible: true,
+        user:{
+            name: 'Wesley Leoncio',
+            email: 'wesley.leoncio.izi@gmail.com'
+        }
     },
 
     mutations:{
